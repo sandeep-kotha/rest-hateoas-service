ed0ecf2b38a06736ff2d161d6e158894ebc29c44
Sandeep Kotha <akotha31@gmail.com>
2016-05-21 12:10:58 +0100

    first commit

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,23 @@
+package hello;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class GreetingController {
+
+    private static final String TEMPLATE = "<status>processed sucessfully</status>";
+
+    @RequestMapping("/subscriber")
+    public HttpEntity<String> greeting(@RequestBody(required=true) String payload) {
+    	System.err.println(payload);
+    	return new ResponseEntity<String>(TEMPLATE, HttpStatus.ACCEPTED);
+    }
+}
